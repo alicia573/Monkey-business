f249e0467bead4c2978a5681a098e845b1247184
alicia573 <54794745+alicia573@users.noreply.github.com>
2020-12-03 15:03:23 +0100

    Opdracht 2

--- index.php
+++ index.php
@@ -1,1 +1,59 @@
-<?php
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>PHP Cursus</title>
+    <link rel="stylesheet" href="index-style.css">
+    <meta charset="UTF-8">
+</head>
+
+<body>
+<div id="wrapper">
+    <header>
+        <h1>PHP Cursus</h1>
+    </header>
+    <table>
+        <tr>
+            <th><a href="index.php">Opdracht 3</a></th>
+        </tr>
+        <tr>
+            <th><a href="Opdracht%202.php">Opdracht 2</a></th>
+
+        </tr>
+        <tr>
+            <th><a href="">Opdracht 3</a></th>
+
+        </tr>
+        <tr>
+            <th><a href="">Opdracht 4</a></th>
+        </tr>
+        <tr>
+            <th><a href="">Opdracht 5</></th>
+
+        </tr>
+        <tr>
+            <th><a href="">Opdracht 6</></th>
+        </tr>
+        <tr>
+            <th><a href="">Opdracht 7</></th>
+
+        </tr>
+        <tr>
+            <th><a href="">Opdracht 8</></th>
+
+        </tr>
+        <tr>
+            <th><a href="">Opdracht 9</></th>
+        </tr>
+    </table>
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
